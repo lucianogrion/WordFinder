911b5fac891ddac97953c61130958e411a6a0430
Lgrion <82838993+lucianogrion@users.noreply.github.com>
2025-03-06 19:46:18 -0300

    Add files via upload

--- MatrixFinder/WordFinder.cs
+++ MatrixFinder/WordFinder.cs
@@ -25,38 +25,47 @@ namespace MatrixFinder
         private Dictionary<char, List<Position>> _dictionaryPosition = new Dictionary<char,List<Position>>();
         private ConcurrentDictionary<string, int> _wordsOccurrencies = new ConcurrentDictionary<string, int>();
 
+        /// <summary>
+        /// Constructor to load the matrix on memory
+        /// </summary>
+        /// <param name="matrix"></param>
         public WordFinder(IEnumerable<string> matrix)
         {
-            int nroLinea = 0;
+            int lineNumber = 0;
 
             var lineas = new List<char[]>();
-
+            //Check the dimension of the Matrix
             CheckMatrix(matrix);
 
             foreach (var item in matrix)
             {
-                char[] linea = item.ToCharArray();
+                char[] line = item.ToCharArray();
                 
                 int nroCol = 0;
-                foreach (char c in linea)
+                foreach (char c in line)
                 {
                     if (!_dictionaryPosition.TryGetValue(c, out var positions))
                     {
                         _dictionaryPosition.Add(c, []);
                     }
                     var list= _dictionaryPosition[c];
-                    list.Add(new Position(nroLinea, nroCol));
+                    list.Add(new Position(lineNumber, nroCol));
                     nroCol++;
                 }
-                lineas.Add(linea);
+                lineas.Add(line);
 
-                nroLinea++;
+                lineNumber++;
             } 
 
             _matrix = lineas.ToArray();
         
         }
 
+        /// <summary>
+        /// Check the dimension of the Matrix
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <exception cref="Exception"></exception>
         private void CheckMatrix(IEnumerable<string> matrix)
         {
             int rows = matrix.Count();
@@ -69,9 +78,14 @@ namespace MatrixFinder
 
         }
 
+        /// <summary>
+        /// The method will find some words 
+        /// </summary>
+        /// <param name="wordstream">The words to seach</param>
+        /// <returns>Ordered list of found words</returns>
         public IEnumerable<string> Find(IEnumerable<string> wordstream)
         {
-
+            //Count/Find each word in parallel
             Parallel.ForEach(wordstream, word =>
             {
                 int CountWords = CountWordsInMatrix(word);
@@ -83,15 +97,21 @@ namespace MatrixFinder
 
         }
 
+        /// <summary>
+        /// CountWordsInMatrix
+        /// </summary>
+        /// <param name="word">the selected word </param>
+        /// <returns>the number of the word appeareances on the matrix</returns>
         private int CountWordsInMatrix(string word)
         {
             int acumulatorCount =0;
-            //get the first letter
+            //get the first letter of the word
             char FirstLetter = word[0]; 
             
-            //If There is no letter should be no word at all text
             if (!_dictionaryPosition.TryGetValue(FirstLetter,out var value))
             {
+                //If There is no letter on the _dictionaryPosition
+                //should be no word at all text
                 return 0;
             }
 
@@ -101,15 +121,17 @@ namespace MatrixFinder
                 acumulatorCount += countPosition;
             }
             return acumulatorCount;
-            
         }
 
+        /// <summary>
+        /// CountInPosition
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
         private int CountInPosition(string word, Position position)
         {
             int count = 0 ;
-            //add limits to the matrix
-            //add vertically and horizontally backwards
-            //make a generic proc (sending parameters)
 
             //search for the word horizontally
             count += SearchForWord(word,true, position);
@@ -118,15 +140,23 @@ namespace MatrixFinder
             return count;
         }
 
+        /// <summary>
+        /// SearchForWord
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="Horizontal"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
         private int SearchForWord(string word,bool Horizontal, Position position)
         {
+
             int Offsetx = 0;
             int Offsety = 0;
             bool founded = true;
             
             foreach (char letter in word)
             {
-                
+                //Check limits of the matrix
                 if ((position.x + Offsetx > _matrix[0].Length - 1)||
                     (position.y + Offsety > _matrix[0].Length - 1)
                     )
@@ -148,9 +178,9 @@ namespace MatrixFinder
                 }
                 
             }
+            //TODO: add vertically and horizontally backwards
 
             return founded ? 1 : 0;
-           
         }
     }
 }
